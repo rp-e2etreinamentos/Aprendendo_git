843d5c4a5f7437f3eba30568f6b35a85414d4a19
rp-e2etreinamentos <114964274+rp-e2etreinamentos@users.noreply.github.com>
2022-11-30 09:49:07 -0300

    Inclusao da class Hello World

---
+++ aprendendo.git/src/main/java/aprendendo/git/HelloWolrd.java
@@ -0,0 +1,12 @@
+package aprendendo.git;
+
+public class HelloWolrd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello Wolrd");	
+	
+	}
+
+}
